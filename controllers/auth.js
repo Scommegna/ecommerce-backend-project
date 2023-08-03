cb7e429d1445f18fc6ea82990e12a5a93f78710c
Lucas Scommegna <scommegnal@hotmail.com>
2023-08-03 11:15:24 -0300

    feat: register user route and validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,19 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+// Register user
 const register = async (req, res) => {
-  res.send("registered");
+  const { email } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists.");
+  }
+
+  const user = await User.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
