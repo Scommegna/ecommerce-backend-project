0954eca6a7cd18f564eb5d8dd85542367e81eb25
Lucas Scommegna <scommegnal@hotmail.com>
2023-08-03 07:31:52 -0300

    feat: basic setup

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+// Connects to mongodb
+const connectDB = require("./db/connect");
+
+// Error Middlewares
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ecommerce-api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+// Starts application
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`app is listening to port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
