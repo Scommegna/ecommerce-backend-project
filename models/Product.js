d9a5e608ab2ae68201fb1d4751a6022bb7803412
Lucas Scommegna <scommegnal@hotmail.com>
2023-08-04 17:28:52 -0300

    feat: aggregation pipeline inital setup

--- models/Product.js
+++ models/Product.js
@@ -58,6 +58,10 @@ const ProductSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
     user: {
       type: mongoose.Types.ObjectId,
       ref: "User",
@@ -67,11 +71,17 @@ const ProductSchema = new mongoose.Schema(
   { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
+// Get all reviews of a product
 ProductSchema.virtual("reviews", {
   ref: "Review",
   localField: "_id",
   foreignField: "product",
   justOne: false,
+});
+
+// Deletes all comments of a product before it is deleted
+ProductSchema.pre("remove", async function () {
+  await this.model("Review").deleteMany({ product: this._id });
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
