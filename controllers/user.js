722e2a83e9dea845b4af20c6bf1b568b22eecd1a
Lucas Scommegna <scommegnal@hotmail.com>
2023-08-03 23:13:02 -0300

    feat: get all users and get single user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,22 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+// Get all users
 const getAllUsers = async (req, res) => {
-  res.send("all users");
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(StatusCodes.OK).json({ users });
 };
 
+// Get single user by id
 const getSingleUser = async (req, res) => {
-  res.send("all users");
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id ${req.params.id}`);
+  }
+
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
