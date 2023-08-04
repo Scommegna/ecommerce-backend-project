718dd8d9f390490cb9f674d9de73b9a8b7548da0
Lucas Scommegna <scommegnal@hotmail.com>
2023-08-04 15:39:25 -0300

    feat: all product controlleras

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const productRouter = require("./routes/product");
 // other packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 // Connects to mongodb
 const connectDB = require("./db/connect");
@@ -23,6 +24,9 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static("./public"));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("ecommerce-api");
--- controllers/product.js
+++ controllers/product.js
@@ -1,27 +1,97 @@
 const Product = require("../models/Product");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const path = require("path");
 
+// Creates product
 const createProduct = async (req, res) => {
-  res.send("created");
+  req.body.user = req.user.userId;
+
+  const product = await Product.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
+// Get all products data
 const getAllProducts = async (req, res) => {
-  res.send("created");
+  const products = await Product.find({});
+
+  res.status(StatusCodes.OK).json({ products, count: products.length });
 };
 
+// Get single product info by id
 const getSingleProduct = async (req, res) => {
-  res.send("created");
+  const { id: productId } = req.params;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id ${productId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
+// Update a product data
 const updateProduct = async (req, res) => {
-  res.send("created");
+  const { id: productId } = req.params;
+
+  const product = await Product.findByIdAndUpdate(
+    { _id: productId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id ${productId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
+// Delete a product data
 const deleteProduct = async (req, res) => {
-  res.send("created");
+  const { id: productId } = req.params;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id ${productId}`);
+  }
+
+  await product.remove();
+
+  res.status(StatusCodes.OK).send();
 };
 
+// Uploads product image
 const uploadImage = async (req, res) => {
-  res.send("created");
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded.");
+  }
+
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please, upload an image.");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please, upload image smaller than 1MB."
+    );
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "..public/uploads" + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
 };
 
 module.exports = {
