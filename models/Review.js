a452606c843ada9b146cffd1259ea45ee599228e
Lucas Scommegna <scommegnal@hotmail.com>
2023-08-07 13:37:30 -0300

    feat: order schema

--- models/Review.js
+++ models/Review.js
@@ -24,7 +24,7 @@ const ReviewSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-    user: {
+    product: {
       type: mongoose.Schema.ObjectId,
       ref: "Product",
       required: true,
@@ -34,5 +34,29 @@ const ReviewSchema = new mongoose.Schema(
 );
 
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+// Calculates average of product reviews rating
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (error) {}
+};
 
 module.exports = mongoose.model("Review", ReviewSchema);
---
+++ models/temp.js
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+import { ObjectId } from "mongodb";
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    $match: {
+      product: new ObjectId("64cd38fec590d30b6a28eb5f"),
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      averageRating: {
+        $avg: "$rating",
+      },
+      numOfReviews: {
+        $sum: 1,
+      },
+    },
+  },
+];
+
+const client = await MongoClient.connect("", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const coll = client.db("ecommerce-api").collection("reviews");
+const cursor = coll.aggregate(agg);
+const result = await cursor.toArray();
+await client.close();
